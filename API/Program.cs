738c7e2a5e68b6911647df91f563f99c30d75eb4
unknown <sami.t.arar@hotmail.com>
2020-08-09 14:42:21 -0400

    Added CRUD using CQRS pattern and MediatR

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Persistence;
@@ -23,10 +18,10 @@ namespace API
             {
                 var services = scope.ServiceProvider;
                 try
-
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
